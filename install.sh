2f1e04fc435868e351661eecc4d701b12b5ddb28
MrKeith99 <kvalentincardenas@hotmail.com>
2025-11-05 22:04:15 +0900

    [UPD] select ros distro branch to pkg

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ CURRENT_DIR=`pwd`
 cd ..
 
 # Download required packages for SOBIT PRO
-git clone https://github.com/TeamSOBITS/sobits_interfaces.git
+git clone -b ${ROS_DISTRO}-devel https://github.com/TeamSOBITS/sobits_interfaces.git
 
 # Go back to previous directory
 cd ${CURRENT_DIR}
@@ -16,16 +16,20 @@ cd ${CURRENT_DIR}
 # Download ROS dependencies
 sudo apt-get update
 sudo apt-get install -y \
-    ros-${ROS_DISTRO}-geometry-msgs \
-    ros-${ROS_DISTRO}-laser-geometry \
+    ros-${ROS_DISTRO}-rclcpp \
+    ros-${ROS_DISTRO}-rclcpp-components \
     ros-${ROS_DISTRO}-pluginlib \
-    ros-${ROS_DISTRO}-pcl-conversions \
-    ros-${ROS_DISTRO}-pcl-ros \
-    ros-${ROS_DISTRO}-visualization-msgs \
+    ros-${ROS_DISTRO}-laser-geometry \
     ros-${ROS_DISTRO}-tf2 \
     ros-${ROS_DISTRO}-tf2-ros \
+    ros-${ROS_DISTRO}-tf2-eigen \
     ros-${ROS_DISTRO}-tf2-geometry-msgs \
-    ros-${ROS_DISTRO}-laser-geometry
+    ros-${ROS_DISTRO}-pcl-conversions \
+    ros-${ROS_DISTRO}-pcl-ros \
+    ros-${ROS_DISTRO}-std-msgs \
+    ros-${ROS_DISTRO}-std-srvs \
+    ros-${ROS_DISTRO}-sensor-msgs \
+    ros-${ROS_DISTRO}-geometry-msgs
 
 
 echo "╚══╣ Install: PCL Object Detection (FINISHED) ╠══╝"
